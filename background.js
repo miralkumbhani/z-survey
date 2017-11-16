212fcd2487b980dda32cedbc40677c8115af5bb3
Miral Kumbhani <miral.kumbhani@zymr.com>
2017-11-16 12:30:46 +0530

    Modified and solved errors and made the code responsive

--- background.js
+++ background.js
@@ -1,21 +1,43 @@
 (function() {
     'use strict';
 
-    const _demographRecord = function() {
-        $('.main').hide();
-    };
+    $(function () {
+        $("#survey-block, #complete, #finish").hide();
+        $('#start').on('click', function() {
+            // console.log('click on start');
+            _demo.init();
+        });
 
-    _demographRecord.prototype = {
-        init: async function() {
+        $('.proceed').on('click', function (){
+            // console.log('click on next, skip');
+            _demo.display();
+        });
+
+        $('#finish').on('click', function () {
+            // console.log('click on finish');
+            _demo.finalMessage();
+        });
+    });
+
+    const _demo = {
+
+        //function displays the main class and loads data for use
+        init: function () {
+            $("#welcome").hide();
+            $("#survey-block").show();
             this.num = 0;
-            this.len = 0;
-            $(".form").hide();
-            $(".main").show();
-
-            this.dataList = await this.fetchData();
-            this.display();
+            this.fetchData().then((result) => {
+                this.dataList = result;
+                this.len = this.dataList.length;
+                this.display();
+            }).catch((err) => {
+                console.error("Rejected", err);
+            });
         },
 
+
+        //function that fetches data from JSON
+        //@returns object {question, [options]}
         fetchData: function() {
             return new Promise((resolve, reject) => {
                 $.getJSON("./survey.json").done((data) => {
@@ -26,31 +48,32 @@
             });
         },
 
+        //function uses dataList and displays the questions and options
         display: function() {
-            //put or check a counter to check whether the question is skipped or answered
-            let len = this.dataList.length;
-            this.len = len;
-            let num = this.num;
-            num = (num >= len) ? 0 : this.num;
-            let question = this.dataList[num].question;
-            let options = this.dataList[num].options;
-            this.num = (this.num >= len) ? 0 : this.num + 1;
-            if (this.num === 0) { //because of the start button
-                this.num = 1;
+
+            this.setProgressBar(this.num);
+            let question = this.dataList[this.num].question;
+            let options = this.dataList[this.num].options;
+
+            this.num = this.num + 1;
+
+            if (this.num === this.len) {
+                $(".button-group").hide();
+                $("#finish").show();
             }
 
-            $('#quesnum').html(this.num + ".");
-            $('#question').html(question);
-            $('#options').html(this.getOptions(options));
-
-            this.setProgressBar(num);
+            $("#quesno").html(this.num);
+            $("#question").html(question);
+            $("#options").html(this.getOptions(options));
         },
 
+        //function sets the radio buttons of options for questions
+        //@returns string of options for each question for HTML
         getOptions: function(options) {
             let radio = '';
             $(options).each(function(index, value) {
-                radio += `<label class="custom-control custom-radio">
-                <input id="radio_${index}" name="radio" type="radio" value="${value}" class="custom-control-input">
+                radio += `<label class="custom-control custom-radio col-md-2">
+                <input id="radio_${index}" name="radio" type="radio" value="${value}" class="custom-control-input cursor-radio">
                 <span class="custom-control-indicator"></span>
                 <span class="custom-control-description option-text">${value}</span>
                 </label>`;
@@ -58,17 +81,20 @@
             return radio;
         },
 
+        //function sets the value for progress bar dynamically
+        //@returns number along with % for HTML to update the progress bar according to the question
         setProgressBar: function(num) {
-            // console.log("num", num);
-            // console.log("this.len", this.len);
-            let cal = (num + 1)* (100 / this.len);
-
-            $('#progress-bar').css("width", function() {
-                // console.log("cal", cal);
+            let cal = (num + 1) * (100 / this.len);
+            $("#progress-bar").css("width", function() {
                 return cal + '%';
             });
+        },
+
+        //function displays the final thank-you message
+        finalMessage: function() {
+            $("#survey-block").hide();
+            $("#complete").show();
         }
     };
 
-    window._demo = new _demographRecord();
 })();
